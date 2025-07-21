d70f1b8cd82fed90f0c66562b5a008fa1546c424
Ayhan <ayhan-games@hotmail.com>
2025-07-22 00:53:54 +0300

    category ve material entitiesler oluşturuldu

---
+++ MiddleEarthTrader.Repository/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarthTrader.Repository.Entities
+{
+    internal class Category
+    {
+    }
+}
---
+++ MiddleEarthTrader.Repository/Entities/Material.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarthTrader.Repository.Entities
+{
+    internal class Material
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Features { get; set; }
+        public decimal BaseValue { get; set; } // Taban fiyat
+        public int CategoryId { get; set; }
+    }
+}
