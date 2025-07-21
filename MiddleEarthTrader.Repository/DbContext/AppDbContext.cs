4c7438352ae85eb25dd6b640c8b96c8afa3502b5
Ayhan <ayhan-games@hotmail.com>
2025-07-22 00:06:18 +0300

    dbcontext eklendi veritabanına baglanmaya hazir hale getirildi

---
+++ MiddleEarthTrader.Repository/DbContext/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiddleEarthTrader.Repository.Entities;
+
+namespace MiddleEarthTrader.Repository.DbContext
+{
+    public class AppDbContext : DbContext
+
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Character> characters { get; set; }
+
+    }
+}
