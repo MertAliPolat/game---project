8ad92eb2993f6ed8f4fb43dbc1561a0a93c48eff
Ayhan <ayhan-games@hotmail.com>
2025-08-08 15:10:56 +0300

    proje düzenlendi

--- MiddleEarthTrader.Service/Dtos/GameEventDto.cs
+++ MiddleEarthTrader.Service/Dtos/GameEventDto.cs
@@ -9,6 +9,7 @@ namespace MiddleEarthTrader.Service.Dtos
 {
     public class GameEventDto
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Type { get; set; }
