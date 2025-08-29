40cc78656306bffb10a6192ebd67730f3610fe18
Ayhan <ayhan-games@hotmail.com>
2025-08-29 16:00:10 +0300

    repository katmanindan is yuku azaltildi

--- MiddleEarthTrader.Repository/Interfaces/IInventoryRepository.cs
+++ MiddleEarthTrader.Repository/Interfaces/IInventoryRepository.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MiddleEarthTrader.Repository.Entities;
-
+﻿using MiddleEarthTrader.Repository.Entities;
 
 namespace MiddleEarthTrader.Repository.Interfaces
 {
     public interface IInventoryRepository
     {
-        Task<IEnumerable<Inventory>> BuyMaterialAsync(Guid userId, Guid materialId, int quantity);
-        Task<IEnumerable<Inventory>> SellMaterialAsync(Guid userId, Guid materialId, int quantity);
+        Task<User?> GetUserByIdAsync(Guid userId);
+        Task<Material?> GetMaterialByIdAsync(Guid materialId);
+        Task<Inventory?> GetInventoryAsync(Guid userId, Guid materialId);
+        Task<IEnumerable<Inventory>> GetUserInventoriesAsync(Guid userId);
+        Task SaveChangesAsync();
     }
 }
--- MiddleEarthTrader.Repository/Repositories/InventoryRepository.cs
+++ MiddleEarthTrader.Repository/Repositories/InventoryRepository.cs
@@ -5,7 +5,6 @@ using MiddleEarthTrader.Repository.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MiddleEarthTrader.Repository.Repositories
@@ -18,100 +17,25 @@ namespace MiddleEarthTrader.Repository.Repositories
         {
             _context = context;
         }
-        public async Task<IEnumerable<Inventory>> BuyMaterialAsync(Guid userId, Guid materialId, int quantity)
-        {
-            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
-            if (user == null)
-                throw new Exception("User not found");
 
-            var material = await _context.Materials.FirstOrDefaultAsync(m => m.Id == materialId);
-            if (material == null)
-                throw new Exception("Material not found");
+        public async Task<User?> GetUserByIdAsync(Guid userId) =>
+            await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
 
-            if (material.AvailableStock < quantity)
-                throw new Exception("Not enough stock available");
+        public async Task<Material?> GetMaterialByIdAsync(Guid materialId) =>
+            await _context.Materials.FirstOrDefaultAsync(m => m.Id == materialId);
 
-            var totalCost = material.CurrentPrice * quantity;
-            if (user.Gold < totalCost)
-                throw new Exception("Not enough gold to buy this material");
-
-            user.Gold -= totalCost;
-            _context.Users.Update(user);
-
-            material.AvailableStock -= quantity;
-            _context.Materials.Update(material);
-
-            var inventory = await _context.Inventories
+        public async Task<Inventory?> GetInventoryAsync(Guid userId, Guid materialId) =>
+            await _context.Inventories
+                .Include(i => i.Material)
                 .FirstOrDefaultAsync(i => i.UserId == userId && i.MaterialId == materialId);
 
-            if (inventory != null)
-            {
-                inventory.Quantity += quantity;
-                _context.Inventories.Update(inventory);
-            }
-            else
-            {
-                var newInventory = new Inventory
-                {
-                    UserId = userId,
-                    MaterialId = materialId,
-                    Quantity = quantity,
-                    AveragePurchasePrice = 0
-                };
-
-                await _context.Inventories.AddAsync(newInventory);
-            }
-
-            await _context.SaveChangesAsync();
-
-            return await _context.Inventories
+        public async Task<IEnumerable<Inventory>> GetUserInventoriesAsync(Guid userId) =>
+            await _context.Inventories
                 .Include(i => i.Material)
                 .Where(i => i.UserId == userId)
                 .ToListAsync();
-        }
 
-
-        public async Task<IEnumerable<Inventory>> SellMaterialAsync(Guid userId, Guid materialId, int quantity)
-        {
-            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
-            if (user == null)
-                throw new Exception("User not found");
-
-            var material = await _context.Materials.FirstOrDefaultAsync(m => m.Id == materialId);
-            if (material == null)
-                throw new Exception("Material not found");
-
-            var inventory = await _context.Inventories
-                .FirstOrDefaultAsync(i => i.UserId == userId && i.MaterialId == materialId);
-
-            if (inventory == null || inventory.Quantity < quantity)
-                throw new Exception("Not enough material in inventory to sell");
-
-            var totalGain = material.CurrentPrice * quantity;
-            user.Gold += totalGain;
-            _context.Users.Update(user);
-
-            material.AvailableStock += quantity;
-            _context.Materials.Update(material);
-
-            inventory.Quantity -= quantity;
-            if (inventory.Quantity <= 0)
-            {
-                _context.Inventories.Remove(inventory);
-            }
-            else
-            {
-                _context.Inventories.Update(inventory);
-            }
-
+        public async Task SaveChangesAsync() =>
             await _context.SaveChangesAsync();
-
-            return await _context.Inventories
-                .Include(i => i.Material)
-                .Where(i => i.UserId == userId)
-                .ToListAsync();
-        }
-
     }
-
 }
